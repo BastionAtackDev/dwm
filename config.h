1afb8e3b3a3f2f360e30f6e97f1e4dd86783d8b3
Mihail_Seby <sbypsm@gmail.com>
2025-08-06 14:31:55 +0300

    actualizare dwm

--- config.h
+++ config.h
@@ -43,6 +43,7 @@ static const Rule rules[] = {
 	{ "Librewolf", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "kitty",     NULL,    NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,      NULL,     "python", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
